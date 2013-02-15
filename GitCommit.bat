debbe04d1240579bdda81c3ba59992c76718649e
Thomasims <thomasims3@hotmail.fr>
2013-02-15 12:09:27 +0100

    Stuff again

--- GitCommit.bat
+++
@@ -1,6 +0,0 @@
-D:\Programs\Git\cmd\git.exe add .
-D:\Programs\Git\cmd\git.exe commit -m "auto eclipse commit"
-D:\Programs\Git\cmd\git.exe remote remove origin
-D:\Programs\Git\cmd\git.exe remote add origin https://github.com/Thomasims/ImefOS_UAddons.git
-D:\Programs\Git\cmd\git.exe pull origin master
-pause
